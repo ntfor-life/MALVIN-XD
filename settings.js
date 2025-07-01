630e040630e314d2be2c3f07a332b1a004b79dad
Dacheno <ntforlife308@gmail.com>
2025-07-01 10:36:24 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~460WDCSA#7BRwlXapR-QOlFTvuJ5UDy9SeWGAxt66BoNUC85xIdY",
+SESSION_ID: process.env.SESSION_ID || "malvin~du8kzJwZ#3GpFPQZUUBMb3T-16ClKkGteyLbkKvi_dugycUTFsEE",
 // add your Session Id make sure it starts with malvin~
-PREFIX: process.env.PREFIX || "'",
+PREFIX: process.env.PREFIX || "💞",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
 // add bot name here for menu
@@ -56,7 +56,7 @@ LINK_WHITELIST: "youtube.com,github.com",
 // add alive msg here 
 
 
-        STICKER_NAME: process.env.STICKER_NAME || "the legende",
+        STICKER_NAME: process.env.STICKER_NAME || "mr dacheno",
 // type sticker pack name 
 
         CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "18293511531",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "18292748112",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "ᴍᴀʟᴠɪɴ ᴋɪɴɢ",
